f9fcb48d7eb020e7f390778bb9514e372c948a5f
sandesh poudel <sandesh100poudel@gmail.com>
2022-11-27 15:42:21 +0545

    sixth Handling the Drawer State

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React,{useState}from "react";
 import {Link} from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElement/Backdrop";
 
 
 const MainNavigation = props =>{
+
+
+
+
+const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+const openDrawer =()=>{
+    setDrawerIsOpen(true);
+}
+
+const closeDrawer =()=>{
+    setDrawerIsOpen(false);
+}
+
+
+
+
     return(
     <React.Fragment> 
-        <SideDrawer>
+{drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+
+     {drawerIsOpen && (<SideDrawer>
             <nav className="main-navigation__drawer-nav">
                 <NavLinks />
             </nav>
-        </SideDrawer>
+        </SideDrawer>)  }  
     <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
             <span />
             <span />
              <span />
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -3,7 +3,7 @@ import "./SideDrawer.css";
 import ReactDOM  from "react-dom";
 
 const SideDrawer = props =>{
-    const content = <aside className="side-Drawer">
+    const content = <aside className="side-drawer">
         {props.children}
     </aside>
 
