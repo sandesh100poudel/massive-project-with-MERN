b8a2d88faf19db8c717fb38f222d57a39a88c087
sandesh poudel <sandesh100poudel@gmail.com>
2022-12-06 09:23:59 +0545

    26 Adding Auth context for App Widestate Management and Listening to context change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useCallback} from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,9 +12,23 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+const login = useCallback(()=>{
+  setIsLoggedIn(true);
+},[]);
+
+const logout = useCallback(()=>{
+  setIsLoggedIn(false);
+},[]);
+
+
+
   return (
+    <AuthContext.Provider value={{isLoggedIn:isLoggedIn, login:login, logout:logout} }>
     <Router>
       <MainNavigation />
       <main>
@@ -37,6 +51,7 @@ const App = () => {
       </Switch>
       </main>
     </Router>
+    </AuthContext.Provider>
   );
 };
 
